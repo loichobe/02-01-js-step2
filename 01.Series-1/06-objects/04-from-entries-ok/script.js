e5b27517ad3e006581fcf46bd5e28812b8583f64
Nicolas Denoël <nicolas.denoel@gmail.com>
2020-07-12 20:48:43 +0200

    End of Exercise 7 - Class

--- 01.Series-1/06-objects/04-from-entries-ok/script.js
+++ 01.Series-1/06-objects/04-from-entries-ok/script.js
@@ -2,8 +2,10 @@
 
 
 (() => {
-    const keys = ["name", "species", "age", "gender", "color"];
-    const values = ["Skitty", "cat", 9, "female", "tabby"];
-    let result = Object.fromEntries(keys.map((_, i) => [keys[i], values[i]]))
-    console.log(result);
+    document.getElementById('run').addEventListener('click', () => {    
+        const keys = ["name", "species", "age", "gender", "color"];
+        const values = ["Skitty", "cat", 9, "female", "tabby"];
+        let result = Object.fromEntries(keys.map((_, i) => [keys[i], values[i]]))
+        console.log(result);
+    });
 })();
--- 01.Series-1/07-classes/04-getter-setter/script.js
+++
@@ -1,6 +0,0 @@
-// 07-classes/04-getter-setter/script.js - 7.4: getter & setter
-
-
-(() => {
-    // your code here
-})();
