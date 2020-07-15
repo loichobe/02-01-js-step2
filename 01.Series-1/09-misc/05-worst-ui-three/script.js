1ba233b8f7c4650a9027c6b5e71a20a8eb88bcd0
Nicolas Denoël <nicolas.denoel@gmail.com>
2020-07-15 16:42:17 +0200

    End of Exercise 9
    9.5 / 9.6 / 9.7

--- 01.Series-1/09-misc/05-worst-ui-three/script.js
+++
@@ -1,7 +0,0 @@
-// 09-misc/05-worst-ui-three/script.js - 9.5: worst user interface (3) : phone slot
-
-(() => {
-
-    // your code here
-
-})();
--- 01.Series-1/09-misc/06-guess-number/script.js
+++
@@ -1,7 +0,0 @@
-// 09-misc/06-guess-number/script.js - 9.6: guess the number
-
-(() => {
-
-    // your code here
-
-})();
